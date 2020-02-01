d65c1201d1184e40ce96dc386164181eccb199aa
aknkya <akn_kya@hotmail.com>
2020-02-01 18:37:46 +0300

    swagger

---
+++ src/main/java/com/newway/newwayapi/project/ProjectRepository.java
@@ -0,0 +1,9 @@
+package com.newway.newwayapi.project;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProjectRepository extends JpaRepository<Project, Long> {
+
+}
